ea906f1fa03e8802e39a3d1201245601b8ae096e
alex-et2 <bosslamesgin@gmail.com>
2022-12-15 02:35:32 -0800

    This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day)
+{
+	switch (month)
+	{
+		case2:
+			day = 31 + day;
+			break;
+		case3:
+			day = 59 + day;
+			break;
+		case4:
+			day = 90 + day;
+			break;
+		case5:
+			day = 120 + day;
+			break;
+		case6:
+			day = 151 + day;
+			break;
+		case7:
+			day = 181 + day;
+			break;
+		case8:
+			day = 212 + day;
+			break;
+		case9:
+			day = 243 + day;
+			break;
+		case10:
+			day = 273 + day;
+			break;
+		case11:
+			day = 304 + day;
+			break;
+		case12:
+			day = 334 + day;
+			break;
+		default:
+			break;
+	}
+
+	return (day);
+}
